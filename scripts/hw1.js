d63186f563276c802cf0f686c22f39854e5506ca
Colman Webster <roguefiend@yahoo.com>
2020-09-03 03:24:09 -0700

    commit 1

---
+++ scripts/hw1.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+$('#cont2').mouseenter(function(){
+    $('span').html('<p>Just kidding. I actually need money for</p></br> <p>food and shelter. I\'m not into the whole</p></br> <p>"starving artist" thing, and I\'d rather cry in</p></br> <p>a Ferrari.</p>');
+});
+});
